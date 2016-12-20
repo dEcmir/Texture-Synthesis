ec788e5171e918496ae2e8891415d683b2c3daf9
pierredet NA <pierredet@gmail.com>
2016-12-20 19:54:41 +0000

    fourier_loss.py file to add a loss for spectral constraints

---
+++ fourier_loss.py
@@ -0,0 +1,34 @@
+from numpy.fft import fft2, ifft2
+import numpy as np
+
+
+def define_fourier_loss(tex):
+    '''
+    returns a loss function computing the distance
+    to the spectrum of tex
+    Args :
+           - tex a numpy array of size 3, w, w
+    output loss (function x0 -> R) where x0 has the same shape as tex
+    '''
+    # store the fft of the grayscale texture in a variable
+    shape = tex.shape
+    tex = (tex[0] + tex[1] + tex[2]) / 3
+    tex = fft2(tex)
+
+    def loss(x0):
+        x0 = x0.reshape(shape)
+        # we constrain on the spectrum of the grayscale image
+        x0 = (x0[0] + x0[1] + x0[2]) / 3
+
+        spectrum = fft2(x0)
+
+        ortho = tex * np.conj(spectrum)
+        projection = ifft2((ortho / np.linalg.norm(ortho)) * spectrum)
+        # return the square norm of the difference
+        return np.square(projection - tex).sum()
+
+    return loss
+
+
+define_fourier_grad(tex):
+    # a toi de jouer grand fou
