1bfabd7582632f6dfdbbbad3199796838d710311
pdubreuil1 <pierre.dubreuil@telecom-paristech.fr>
2016-12-23 12:48:36 +0100

    Write the gradient part + comment loss part + beautify texture_cnn2

--- fourier_loss.py
+++ fourier_loss.py
@@ -7,7 +7,7 @@ def define_fourier_loss(tex):
     returns a loss function computing the distance
     to the spectrum of tex
     Args :
-           - tex a numpy array of size 3, w, w
+           - tex a numpy array of size 3, w, w. being the generated texture
     output loss (function x0 -> R) where x0 has the same shape as tex
     '''
     # store the fft of the grayscale texture in a variable
@@ -15,7 +15,8 @@ def define_fourier_loss(tex):
     tex = (tex[0] + tex[1] + tex[2]) / 3
     tex = fft2(tex)
 
-    def loss(x0):
+    def spectrum_loss(x0):
+        # x0 is the generated texture image
         x0 = x0.reshape(shape)
         # we constrain on the spectrum of the grayscale image
         x0 = (x0[0] + x0[1] + x0[2]) / 3
@@ -27,8 +28,34 @@ def define_fourier_loss(tex):
         # return the square norm of the difference
         return np.real(np.square(projection - tex).sum())
 
-    return loss
+    return spectrum_loss
 
 
-define_fourier_grad(tex):
-    # a toi de jouer grand fou
+def define_fourier_grad(tex):
+    ''' From a color image 'x_0' compute its spectrum constraint gradient
+        in regard to the the original image 'tex'
+        Args:
+            - tex (list): list of the used arms
+        Output:
+            - fourier_grad (function): x0 -> gradient of the spectrum
+                                                   loss
+    '''
+
+    # store the fft of the grayscale texture in a variable
+    shape = tex.shape
+    tex = (tex[0] + tex[1] + tex[2]) / 3
+    tex = fft2(tex)
+
+    def fourier_grad(x0):
+        # x0 is the generated texture image
+        x0 = x0.reshape(shape)
+        # we constrain on the spectrum of the grayscale image
+        x0 = (x0[0] + x0[1] + x0[2]) / 3
+
+        spectrum = fft2(x0)
+
+        ortho = tex * np.conj(spectrum)
+        projection = ifft2((ortho / np.linalg.norm(ortho)) * spectrum)
+        return x0 - projection
+
+    return fourier_grad
